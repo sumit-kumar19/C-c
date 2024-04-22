6e262420b6c1e16116156c3f6f1e4e51e90b5953
sumit-kumar19 <143984750+sumit-kumar19@users.noreply.github.com>
2024-04-22 12:12:41 +0530

    commit

---
+++ c++/cses/apartment.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std ;
+#define ll long long
+void solve(){
+    int n;
+    cin>>n;
+
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+}
