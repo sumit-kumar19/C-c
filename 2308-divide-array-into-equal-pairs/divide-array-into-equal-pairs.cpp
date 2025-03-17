11960b0a877e5da2dfceaedf6a0e4169ff10c905
Sumit Kumar <143984750+sumit-kumar19@users.noreply.github.com>
2025-03-17 09:16:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (66.52%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -4,13 +4,14 @@ public:
         int maxelement = *max_element(nums.begin(),nums.end());
 
         vector<int> count(maxelement+1,0);
-        for(int num:nums) {
-            count[num]++;
+    for_each(nums.begin(), nums.end(), [&](int num) { count[num]++; });
+        // for(int i=0;i<=maxelement ; i++ ) {
+        //     if(count[i]%2!=0)
+        //     return false;
+      if (any_of(count.begin(), count.end(), [](int val) {return val%2!=0;})) {
+        return false;
         }
-        for(int i=0;i<=maxelement ; i++ ) {
-            if(count[i]%2!=0)
-            return false;
-        }
+
         return true;
     }
 };
