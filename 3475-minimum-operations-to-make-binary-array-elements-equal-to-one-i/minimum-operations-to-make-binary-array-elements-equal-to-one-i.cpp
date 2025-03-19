7e48189fa64633beb86740347e6e15d9dfffedac
Sumit Kumar <143984750+sumit-kumar19@users.noreply.github.com>
2025-03-19 09:36:51 +0530

    Time: 12 ms (26.22%) | Memory: 140 MB (74.50%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -3,14 +3,21 @@ public:
     int minOperations(vector<int>& nums) {
     int count =0 ;
     int it1=2;
-    while(it1<nums.size()){
-        if(nums[it1-2]==0){
-            nums[it1]=!nums[it1];
-            nums[it1-2]=!nums[it1-2];
-            nums[it1-1]=!nums[it1-1];
-            count++;}
+    auto processFlips = [&]() {
+        int it1 = 2;
+        while (it1 < nums.size()) {
+            if (nums[it1 - 2] == 0) {
+                nums[it1] = !nums[it1];
+                nums[it1 - 2] = !nums[it1 - 2];
+                nums[it1 - 1] = !nums[it1 - 1];
+                count++;
+            }
             it1++;
-    }
+        }
+    };
+    processFlips();
+
+
     if (any_of(nums.begin(), nums.end(), [](int val) {return val==0;})) {
         return -1;
         }
